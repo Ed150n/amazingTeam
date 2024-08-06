3fb8b25c9f6775af30afcb347bbe30138a406217
xavierp10 <franciscoporras1000@gmail.com>
2024-08-06 07:07:39 -0500

    Actualizacion

--- Aplicaciones/Gestion/urls.py
+++ Aplicaciones/Gestion/urls.py
@@ -6,4 +6,14 @@ from .import views
 # Crear un arreglo
 urlpatterns = [ 
     path('inicio/',views.inicio, name='inicio'),
+    
+    #****************************************************USUARIO*********************************************************
+    # LISTADO USUARIOS
+    path('listadoUsuarios/',views.listadoUsuarios, name='listadoUsuarios'),
+    # FORMULARIO NUEVO USUARIO
+    path('nuevoUsuario/',views.nuevoUsuario, name='nuevoUsuario'),
+    # INSERTAR USUARIO
+    path('insertarUsuario/',views.insertarUsuario, name='insertarUsuario'),
+    # FORMULARIO EDITAR USUARIO
+    path('editarUsuario/<idUsuario>',views.editarUsuario, name='editarUsuario'),
 ]
--- Aplicaciones/Gestion/views.py
+++ Aplicaciones/Gestion/views.py
@@ -4,6 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Se usa para que solo usuarios autenticados pueden acceder a un template
 from django.contrib.auth.decorators import login_required
+# Muestra mensajes
+from django.contrib import messages
 
 #Importar los modelos
 from Aplicaciones.Home.models import Usuario # Model Usuario de Home
@@ -17,4 +19,30 @@ def inicio(request):
 @login_required
 def listadoUsuarios(request):
     usuariosBdd = Usuario.objects.all()
-    return render(request,"listadoUsuarios.html",{'usuarios':usuariosBdd})
+    return render(request,"Usuario/listadoUsuarios.html",{'usuarios':usuariosBdd})
+#RENDERIZAR FORMULARIO AGREGAR USUARIO
+def nuevoUsuario(request):
+    return render(request,'Usuario/nuevoUsuario.html')
+# INSERTAR USUARIO
+def insertarUsuario(request):
+    primerNombre = request.POST["primerNombre"]
+    segundoNombre = request.POST["segundoNombre"]
+    apellidoPaterno = request.POST["apellidoPaterno"]
+    apellidoMaterno = request.POST["apellidoMaterno"]
+    direccion = request.POST["direccion"]
+    telefono = request.POST["telefono"]
+    foto = request.POST.get("foto")
+    email = request.POST["email"]
+    usuario = request.POST["usuario"]
+    contraseña = request.POST["contraseña"]
+    rol = request.POST["rol"]
+    Usuario.objects.create(primerNombre=primerNombre,segundoNombre=segundoNombre,apellidoPaterno=apellidoPaterno,
+                                          apellidoMaterno=apellidoMaterno,direccion=direccion,telefono=telefono,fotografia=foto,
+                                          email=email,usuario=usuario,contraseña=contraseña,rol=rol)
+    messages.success(request,"Usuario registrado exitosamente")
+    return redirect('nuevoUsuario')
+#RENDERIZAR FORMULARIO EDITAR USUARIO
+def editarUsuario(request,idUsuario):
+    usuarioEditar = Usuario.objects.get(id=idUsuario)
+    return render(request,'Usuario/editarUsuario.html',{'usuarioEditar':usuarioEditar})
+# EDITAR USUARIO
